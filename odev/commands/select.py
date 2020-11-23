92e3d0a501f591e68cf3eeb81ed35ca6b9d6f0e1
Joseph Caburnay <jcb@odoo.com>
2020-11-23 14:09:27 +0100

    new select

--- odev/commands/select.py
+++ odev/commands/select.py
@@ -4,12 +4,19 @@ from ..main import main
 
 
 @main.command("select")
-@click.argument("index", type=int)
+@click.option("-s", "--search-string")
+@click.option("-i", "--index", default=0)
 @click.pass_obj
-def select(obj, index):
-    worktrees = obj.list_worktrees()
+def select(obj, search_string, index=0):
+    worktrees = obj.list_worktrees(search_string)
+    if len(worktrees) == 0:
+        click.echo(f"No result for '{search_string}'.")
+        exit(1)
+    if len(worktrees) == 1:
+        index = 0
     if index >= len(worktrees):
         click.echo("Invalid index.", err=True)
         exit(1)
     selected = worktrees[index]
     obj.set_current(selected)
+    click.echo(f"{selected} is selected.")
